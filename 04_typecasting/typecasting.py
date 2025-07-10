72ecf8789c06ec1089bbdd0a825ea77218ebce8d
MuneebAhmad03 <muneeebahmad.03@gmail.com>
2025-07-10 23:41:31 +0500

    File added

---
+++ 04_typecasting/typecasting.py
@@ -0,0 +1,22 @@
+
+
+a = 34              #intially its integer
+
+b = "11"            #initially its String
+
+
+c = str(a)          #Converted integer to String
+
+d = int(b)          #Converted String to integer
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
+
+print(d)
+print(type(d))
